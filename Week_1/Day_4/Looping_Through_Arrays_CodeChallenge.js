198012949865ff0d7db38a38f009f562ac068a87
Brignie Mendieta <51513194+brimendi@users.noreply.github.com>
2022-07-11 12:05:35 -0700

    day1 of week 2 work

---
+++ Week_1/Day_4/Looping_Through_Arrays_CodeChallenge.js
@@ -0,0 +1,37 @@
+// #1
+var arr1 = [8, 6, 7, 5, 3, 0, 9];
+// #2
+var arr2 = [4, 7, 13, 13, 19, 37, -2];
+// #3
+var arr3 = [6, 2, 12, 14, -24, 5, 0];
+
+for(var i = 0; i<arr1.length; i++){
+    console.log(arr1[i]);
+}
+
+console.log("-----");
+
+for(var i = 0; i<arr2.length; i++){
+    var sum = i + arr2[i];
+    console.log(sum)
+}
+
+console.log("-----");
+
+for(var i = 0; i<arr3.length; i++){
+    if(arr3[i] > 5)
+        console.log(arr3[i])
+
+}
+
+console.log("----")
+
+
+for(var i = 0; i<arr3.length; i++){
+    if(arr3[i] > 5)
+        console.log(arr3[i])
+
+    else 
+        arr3[i] = "No dice";
+    
+}
